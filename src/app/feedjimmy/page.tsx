87d221cf91f876c72401ce3bd06723db8fb61342
Jon tarrant <jon.tarrant@dunelm.com>
2025-03-07 13:10:46 +0000

    Updating to add back button

--- src/app/feedjimmy/page.tsx
+++ src/app/feedjimmy/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PoseDectector from "./PoseDectector";
 
 export default function Home() {
@@ -5,6 +6,12 @@ export default function Home() {
     <main>
         <h1 className="text-center text-3xl font-bold mb-4">Feed Jimmy the snake</h1>
         <PoseDectector />
+        <Link 
+            href="/" 
+            className="absolute top-4 right-4 px-6 py-3 bg-gray-700 hover:bg-gray-600 text-white font-semibold text-md rounded-lg shadow-md transition-all duration-300"
+        >
+            ⬅ Back to Menu
+        </Link>
     </main>
   );
 }
